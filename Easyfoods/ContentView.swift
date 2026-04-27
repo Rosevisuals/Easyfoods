5c6c5cb5020610c418fa4a4d86b2ab65de08d792
Namubiru Rose <145221322+Rosevisuals@users.noreply.github.com>
2026-04-27 20:07:38 +0300

    Initial Commit

---
+++ Easyfoods/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Easyfoods
+//
+//  Created by Rose Visuals on 27/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Easyfoods/EasyfoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyfoodsApp.swift
+//  Easyfoods
+//
+//  Created by Rose Visuals on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct EasyfoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
